5dc9df78cf3d03d96e323df48982fd658d3bba91
lutfiibnm <lutfiibnu2908@gmail.com>
2026-05-15 01:33:16 +0700

    Update sw.js

--- public/sw.js
+++ public/sw.js
@@ -1,19 +1,16 @@
-const CACHE_NAME = "focusnote-pomodoro-v2";
+const CACHE_NAME = "focusnote-pomodoro-v99";
 
-self.addEventListener("install", (event) => {
+self.addEventListener("install", () => {
   self.skipWaiting();
 });
 
 self.addEventListener("activate", (event) => {
   event.waitUntil(
     caches.keys().then((cacheNames) => {
-      return Promise.all(
-        cacheNames
-          .filter((name) => name !== CACHE_NAME)
-          .map((name) => caches.delete(name))
-      );
+      return Promise.all(cacheNames.map((name) => caches.delete(name)));
     })
   );
+
   self.clients.claim();
 });
 
@@ -21,24 +18,8 @@ self.addEventListener("fetch", (event) => {
   if (event.request.method !== "GET") return;
 
   event.respondWith(
-    caches.match(event.request).then((cachedResponse) => {
-      if (cachedResponse) {
-        return cachedResponse;
-      }
-
-      return fetch(event.request)
-        .then((networkResponse) => {
-          const responseClone = networkResponse.clone();
-
-          caches.open(CACHE_NAME).then((cache) => {
-            cache.put(event.request, responseClone);
-          });
-
-          return networkResponse;
-        })
-        .catch(() => {
-          return caches.match("./index.html");
-        });
+    fetch(event.request).catch(() => {
+      return caches.match(event.request);
     })
   );
 });
